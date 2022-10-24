b362dc136ee1498f7d30021b603d9edc7aacbb95
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-10-24 09:13:22 -0400

    getting employees and their roles in querries

---
+++ routes/apiRoutes/employees.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const inputCheck = require("../../utils/inputCheck");
+const db = require("../../config/connection");
+
+router.get("/employees", (req, res) => {
+  const sql = "SELECT";
+});
+
+module.exports = router;
