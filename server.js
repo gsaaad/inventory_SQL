9e80b4283ef257e7dac3de4985ea722efe55984e
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-13 06:35:01 -0500

    fixing bugs to connect to sql and adding routes for departments, roles, inventory, employees

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const inputCheck = require("./utils/inputCheck");
 require("dotenv").config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const db = require("./config/connection");
@@ -10,13 +11,22 @@ const apiRoutes = require("./routes/apiRoutes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api", apiRoutes);
+app.use(" /api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Listening to server on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Listening to server on ${PORT}`);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
+});
+
+db.connect((err) => {
+  if (err) throw err;
+
+  console.log("SQL database connected~!");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
