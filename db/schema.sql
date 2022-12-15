2e4806ac63c53b34d56b7ab9e83b66cefe9d0d0b
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-15 16:55:08 -0500

    added an updated schema for inventory, departments roles and employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,42 +1,59 @@
 
 DROP TABLE IF EXISTS inventory;
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
 
 
 
 
 create TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    extension INTEGER,
     name VARCHAR(30) NOT NULL,
-    description TEXT
+    description TEXT,
+    budget FLOAT,
+    sale TEXT,
+    margin FLOAT,
+    trending TEXT
 );
 
 /* Create Table for Inventory */
 CREATE TABLE inventory (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    code INTEGER,
+    code INTEGER NOT NULL,
     name VARCHAR(30) NOT NULL,
-    qty INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
     price FLOAT NOT NULL,
-    family VARCHAR(30) NOT NULL,
+    category VARCHAR(30) NOT NULL,
     department INTEGER,
     inSTOCK BOOLEAN NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY(department) REFERENCES departments(id) ON DELETE SET NULL
+    supplier VARCHAR(30) NOT NULL,
+    country VARCHAR(30) NOT NULL,
+    lastShipment DATETIME,
+    nextShipment DATETIME
+    /* CONSTRAINT fk_department FOREIGN KEY(department) REFERENCES departments(id) ON DELETE SET NULL */
 );
 
 CREATE TABLE roles(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary FLOAT,
-    department INTEGER
-    /* CONSTRAINT fk_department FOREIGN KEY(department) REFERENCES departments(id) ON DELETE SET NULL */
+    department INTEGER,
+    responsibilities TEXT,
+    positionType TEXT,
+    reportsTo INTEGER REFERENCES employees(id) ON DELETE SET NULL
+    /* CONSTRAINT fk_roles_department FOREIGN KEY(department) REFERENCES departments(id) ON DELETE SET NULL */
 );
 
 CREATE TABLE employees(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    employeeNumber INTEGER(11),
+    extension INTEGER(11),
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER REFERENCES employee(id) ON DELETE SET NULL
+    email VARCHAR(100) NOT NULL,
+    role_id INTEGER
     /* CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL */
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,37 +1,37 @@
 
 /* Adding values in inventory Data */
-
-INSERT INTO inventory (code, name, qty, price, family, department, inSTOCK)
+/* 
+INSERT INTO inventory (code, name, quantity, price, category, department, inSTOCK, supplier, country, lastShipment, nextShipment)
 VALUES
-(4011,'Yellow Banana', 15, 3.99, 'Banana',1, 1),    
-(4022,'Plantain Banana', 5, 6.99, 'Banana',1, 1),
-(4033,'Blue Banana',15, 6.99, 'Banana',1, 1),
-(4044,'Brown Banana', 15, 4.99, 'Banana',1, 1),
-(4100,'Granny Smith Apples', 5, 6.99, 'Apples',1, 1),
-(4200,'Red Delicious Apples', 15, 6.99, 'Apples',1, 1),
-(4300,'PinkLady Apples', 35, 3.99, 'Apples',1, 1),
-(4400,'HoneyCrisp Apples', 5, 6.99, 'Apples',1, 1),
-(4500,'Fuji Apples', 51, 1.99, 'Apples',1, 1),
-(4600,'Orange', 35, 6.99, 'Citrus',1, 1),
-(4700,'Clementine',5, 6.99,'Citrus',1, 1),
-(4800,'Lime',25,6.99,'Citrus',1, 1),
-(4900,'Lemon',5,4.99, 'Citrus',1, 1),
-(5000,'Tangerine',15,2.99, 'Citrus',1, 1),
-(5100,'Grape Fruit',5, 4.99, 'Citrus',1, 1),
-(6000,'PS4',10, 450.99, 'PlayStation',8, 1),
-(6100,'PS5',15, 650.99, 'PlayStation',8, 1),
-(6200,'PS6',10, 990.99, 'PlayStation',8, 1),
-(6300,'XBOX',10, 550.99, 'XBOX',8, 1),
-(6400,'XBOX360',15, 250.99, 'XBOX',8, 1),
-(6500,'XBOX720',22, 770.99, 'XBOX',8, 1);
+(4011,'Yellow Banana', 15, 3.99, 'Banana',1, 1, "Banana Corp", "Argentina", "2020-03-14 00:00:13", "2025-05-05 10:15:25"  ),  
+(4022,'Plantain Banana', 5, 6.99, 'Banana',1, 1, "Banana Corp", "Argentina", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4033,'Blue Banana',15, 6.99, 'Banana',1, 1, "Banana Corp", "Argentina", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4044,'Brown Banana', 15, 4.99, 'Banana',1, 1, "Banana Corp", "Argentina", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4100,'Granny Smith Apples', 5, 6.99, 'Apples',1, 1, "Apple Corp", "Canada", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4200,'Red Delicious Apples', 15, 6.99, 'Apples',1, 1, "Apple Corp", "Canada", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4300,'PinkLady Apples', 35, 3.99, 'Apples',1, 1, "Apple Corp", "Canada", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4400,'HoneyCrisp Apples', 5, 6.99, 'Apples',1, 1, "Apple Corp", "Canada", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4500,'Fuji Apples', 51, 1.99, 'Apples',1, 1, "Apple Corp", "Canada", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(4600,'Orange', 35, 6.99, 'Citrus',1, 1, "Citrus Corp", "USA", "2020-03-14 00:25:13", "2025-05-05 10:15:25"),
+(4700,'Clementine',5, 6.99,'Citrus',1, 1, "Citrus Corp", "USA", "2020-03-14 00:25:13", "2025-05-05 10:15:25"),
+(4800,'Lime',25,6.99,'Citrus',1, 1, "Citrus Corp", "USA", "2020-03-14 00:25:13", "2025-05-05 10:15:25"),
+(4900,'Lemon',5,4.99, 'Citrus',1, 1, "Citrus Corp", "USA", "2020-03-14 00:25:13", "2025-05-05 10:15:25"),
+(5000,'Tangerine',15,2.99, 'Citrus',1, 1, "Citrus Corp", "USA", "2020-03-14 00:25:13", "2025-05-05 10:15:25"),
+(5100,'Grape Fruit',5, 4.99, 'Citrus',1, 1, "Citrus Corp", "USA", "2020-03-14 00:25:13", "2025-05-05 10:15:25"),
+(6000,'PS4',10, 450.99, 'PlayStation',8, 1, "SONE Corp", "JAPAN", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(6100,'PS5',15, 650.99, 'PlayStation',8, 1, "SONE Corp", "JAPAN", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(6200,'PS6',10, 990.99, 'PlayStation',8, 1, "SONE Corp", "JAPAN", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(6300,'XBOX',10, 550.99, 'XBOX',8, 1, "Micro Corp", "USA", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(6400,'XBOX360',15, 250.99, 'XBOX',8, 1, "Micro Corp", "USA", "2020-03-14 00:00:13", "2025-05-05 10:15:25"),
+(6500,'XBOX720',22, 770.99, 'XBOX',8, 1, "Micro Corp", "USA", "2020-03-14 00:00:13", "2025-05-05 10:15:25");
+ */
 
 
 
 
 
 
-
-INSERT INTO departments (name, description)
+/* INSERT INTO departments (name, description)
 VALUES
 ('Produce', 'Fruits, vegetables, Fresh Smoothies'),
 ('Clothing', 'T-Shirts, Pants, Socks'),
@@ -40,9 +40,9 @@ VALUES
 ('Meat', 'Ribs, Steak,Ground-Beef'),
 ('SeasFood','Salmon, Trout, Squid'),
 ('Baby', 'Diapers, Toys, Pacifiers'),
-('Electronics', 'PS5, XBOX360, Wii');
+('Electronics', 'PS5, XBOX360, Wii'); */
 
-
+/* 
 INSERT INTO roles (title, salary, department)
 VALUES
 ('Manager', 85000, 1),
@@ -68,55 +68,12 @@ VALUES
 ("Employee", 25000, 7),
 ('Manager', 90000, 8),
 ('Assistant Manager', 37000, 8),
-("Employee", 25000, 8);
+("Employee", 25000, 8); */
 
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+/* INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
 ('Josh','Jacobs', 1,  1 ),
 ('Mike','Johnson', 2, 1 ),
 ('Ja','Morant', 3,  1 ),
-('Ja','Morant', 4,  2 ),
-('Ja','Morant', 5,  2 ),
-('Ja','Morant', 6,  2 ),
-('Ja','Morant', 7,  3 ),
-('Ja','Morant', 8,  3 ),
-('Ja','Morant', 9,  3 ),
-('Ja','Morant', 10, 4 ),
-('Ja','Morant', 11, 4 ),
-('Ja','Morant', 12, 4 ),
-('Ja','Morant', 13 ,5 ),
-('Ja','Morant', 14, 5 ),
-('Ja','Morant', 15, 5 ),
-('Ja','Morant', 16, 6 ),
-('Ja','Morant', 17, 6 ),
-('Ja','Morant', 18, 6 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 20, 7 ),
-('Ja','Morant', 21, 7 ),
-('Ja','Morant', 22, 7 ),
-('Ja','Morant', 23, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 ),
-('Ja','Morant', 19, 7 );
-
-
+('Ja','Morant', 4,  2 ), */
